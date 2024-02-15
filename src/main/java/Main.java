628036a04eb14a04748a07da2fe99647d11bbcb5
samangishamim <samangi624@gmail.com>
2024-02-15 21:47:36 +0330

    test again

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args) {
         MyHashMap<String, String> myHashMap = new MyHashMap<>();
         myHashMap.put("A", "Apple");
+        myHashMap.put("A","APPLE2");
         myHashMap.put("B", "Banana");
         myHashMap.put("C", "Cherry");
+        myHashMap.put("E", "Cherry");
+
 
         System.out.println("Contains key 'A': " + myHashMap.containsKey("A"));
         System.out.println();
