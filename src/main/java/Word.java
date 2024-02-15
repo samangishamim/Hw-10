c8bc06018cba991d1b3ef21190b4e59b14a21f11
samangishamim <samangi624@gmail.com>
2024-02-15 20:45:19 +0330

    add hashmap and complete te main

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,9 +1,21 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Word {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the word : ");
+        String word = scanner.nextLine();
 
+
+        HashMap<Character, Integer> letter = new HashMap<>();
+
+        for (char a : word.toCharArray()) {
+            letter.put(a, letter.getOrDefault(a, 0) + 1);
+        }
+        generate("", word.length(), letter);
     }
+
     private static void generate(String p, int r, HashMap<Character, Integer> letter) {
         if (r == 0) {
             System.out.println(p);
