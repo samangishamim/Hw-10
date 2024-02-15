a99428ad0216cb513d919416e70b39f857edaa48
samangishamim <samangi624@gmail.com>
2024-02-15 20:44:51 +0330

    create generate method for checking the letters

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,2 +1,23 @@
+import java.util.HashMap;
+
 public class Word {
+    public static void main(String[] args) {
+
+    }
+    private static void generate(String p, int r, HashMap<Character, Integer> letter) {
+        if (r == 0) {
+            System.out.println(p);
+            return;
+        }
+
+        for (char c : letter.keySet()) {
+            int count = letter.get(c);
+            if (count > 0) {
+                letter.put(c, count - 1);
+                generate(p + c, r - 1, letter);
+                letter.put(c, count);
+            }
+        }
+
+    }
 }
