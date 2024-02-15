40ff35e45baffa5ea1af8089f5a1ad99448152a5
samangishamim <samangi624@gmail.com>
2024-02-15 20:54:52 +0330

    write and test all the methods for myHashMap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,20 @@
 public class Main {
+    public static void main(String[] args) {
+        MyHashMap<String, String> myHashMap = new MyHashMap<>();
+        myHashMap.put("A", "Apple");
+        myHashMap.put("B", "Banana");
+        myHashMap.put("C", "Cherry");
+
+        System.out.println("Contains key 'A': " + myHashMap.containsKey("A"));
+        System.out.println();
+        System.out.println("Is HashMap empty: " + myHashMap.isEmpty());
+        System.out.println();
+        System.out.println("All values: " + myHashMap);
+        System.out.println("Value for key 'B': " + myHashMap.get("B"));
+
+        System.out.println();
+        myHashMap.replace("A", "wow");
+        System.out.println("Value for key 'A' after replacement: " + myHashMap.get("A"));
+        System.out.println("All values: " + myHashMap);
+    }
 }
