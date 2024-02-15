6732ab174fa52c8dc144226c84a36de237cf11ca
samangishamim <samangi624@gmail.com>
2024-02-15 20:51:36 +0330

    complete the main part for user input and use the methods

--- src/main/java/MyTreeSet.java
+++ src/main/java/MyTreeSet.java
@@ -3,7 +3,14 @@ import java.util.TreeSet;
 
 public class MyTreeSet {
     public static void main(String[] args) {
-
+        TreeSet<Character> set1 = getRandomCharacterSet();
+        TreeSet<Character> set2 = getRandomCharacterSet();
+        System.out.println("Set 1: " + set1);
+        System.out.println("Set 2: " + set2);
+        TreeSet<Character> unionSet = findUnion(set1, set2);
+        System.out.println("Union Set: " + unionSet);
+        TreeSet<Character> commonSet = findCommonality(set1, set2);
+        System.out.println("Common Set: " + commonSet);
     }
 
     public static TreeSet<Character> getRandomCharacterSet() {
