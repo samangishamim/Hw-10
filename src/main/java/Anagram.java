4dc3ab5264dbcb54b9db2e3e538b49591b238617
samangishamim <samangi624@gmail.com>
2024-02-15 20:46:47 +0330

    add method for checking the anagrams

--- src/main/java/Anagram.java
+++ src/main/java/Anagram.java
@@ -1,2 +1,21 @@
+import java.util.HashMap;
+
 public class Anagram {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean checkAnagrams(String word1, String word2) {
+        HashMap<Character, Integer> charCount1 = new HashMap<>();
+        for (char c : word1.toCharArray()) {
+            charCount1.put(c, charCount1.getOrDefault(c, 0) + 1);
+        }
+
+        HashMap<Character, Integer> charCount2 = new HashMap<>();
+        for (char c : word2.toCharArray()) {
+            charCount2.put(c, charCount2.getOrDefault(c, 0) + 1);
+        }
+
+        return charCount1.equals(charCount2);
+    }
 }
