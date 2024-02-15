e00ecb0a774e4b9fcf7d1dbb460944bc515bf515
samangishamim <samangi624@gmail.com>
2024-02-15 20:49:28 +0330

    write a method for getting the random characters

--- src/main/java/MyTreeSet.java
+++ src/main/java/MyTreeSet.java
@@ -1,2 +1,17 @@
+import java.util.Random;
+import java.util.TreeSet;
+
 public class MyTreeSet {
+    public static void main(String[] args) {
+
+    }
+    public static TreeSet<Character> getRandomCharacterSet() {
+        TreeSet<Character> set = new TreeSet<>();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            char randomChar = (char) (random.nextInt(26) + 'a');
+            set.add(randomChar);
+        }
+        return set;
+    }
 }
